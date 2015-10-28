97b8b00db3401b39a84773f41dd1e928d3210713
mdollx <littlemitsuko@gmail.com>
2015-10-28 03:31:12 -0500

    CSS/Date
    Cleaned up css a bit, added date picker to go with scheduling form.
    Added ervices drop down menu in form.

--- app.js
+++ app.js
@@ -3,21 +3,17 @@ angular.module('scheduleApp', ['firebase'])
 .controller('mainController', function($scope, $firebase) {
   
   
- var ref = new Firebase("https://blazing-inferno-1033.firebaseio.com/days");  
-  var fb = $firebase(ref);
+var ref = new Firebase("https://blazing-inferno-1033.firebaseio.com/days");  
+var fb = $firebase(ref);
+var syncObject = fb.$asObject();
 
-  // sync as object 
-  var syncObject = fb.$asObject();
-
-  // three way data binding
-  syncObject.$bindTo($scope, 'days');
+ syncObject.$bindTo($scope, 'days');
   
-    // function to set the default data
-  $scope.reset = function() {    
+   $scope.reset = function() {    
 
     fb.$set({
       01: {
-        name: 'Mon',
+        name: 'Times',
         slots: {
           "900": {
             time: '9:00am',
@@ -46,142 +42,19 @@ angular.module('scheduleApp', ['firebase'])
           "1500": {
             time: '3:00pm',
             booked: false
+          },
+         "1600": {
+            time: '4:00pm',
+            booked: false
+          },
+          "1700": {
+            time: '5:00pm',
+            booked: false
+        
           }
-        }
-      },
-      02: {
-        name: 'Tue',
-        slots: {
-          "900": {
-            time: '9:00am',
-            booked: false
-          },
-          "1000": {
-            time: '10:00am',
-            booked: false
-          },
-          "1100": {
-            time: '11:00am',
-            booked: false
-          },
-          "1200": {
-            time: '12:00pm',
-            booked: false
-          },
-          "1300": {
-            time: '1:00pm',
-            booked: false
-          },
-          "1400": {
-            time: '2:00pm',
-            booked: false
-          },
-          "1500": {
-            time: '3:00pm',
-            booked: false
-           }
-        }
-      },
-      03: {
-        name: 'Wed',
-        slots: {
-          "900": {
-            time: '9:00am',
-            booked: false
-          },
-          "1000": {
-            time: '10:00am',
-            booked: false
-          },
-          "1100": {
-            time: '11:00am',
-            booked: false
-          },
-          "1200": {
-            time: '12:00pm',
-            booked: false
-          },
-          "1300": {
-            time: '1:00pm',
-            booked: false
-          },
-          "1400": {
-            time: '2:00pm',
-            booked: false
-          },
-          "1500": {
-            time: '3:00pm',
-            booked: false
-          }
-        }
-      },
-      04: {
-        name: 'Thurs',
-        slots: {
-          "900": {
-            time: '9:00am',
-            booked: false
-          },
-          "1000": {
-            time: '10:00am',
-            booked: false
-          },
-          "1100": {
-            time: '11:00am',
-            booked: false
-          },
-          "1200": {
-            time: '12:00pm',
-            booked: false
-          },
-          "1300": {
-            time: '1:00pm',
-            booked: false
-          },
-          "1400": {
-            time: '2:00pm',
-            booked: false
-          },
-          "1500": {
-            time: '3:00pm',
-            booked: false
-          }
-        }
-      },
-      05: {
-       name: 'Fri',
-        slots: {
-          "900": {
-            time: '9:00am',
-            booked: false
-          },
-          "1000": {
-            time: '10:00am',
-            booked: false
-          },
-          "1100": {
-            time: '11:00am',
-            booked: false
-          },
-          "1200": {
-            time: '12:00pm',
-            booked: false
-          },
-          "1300": {
-            time: '1:00pm',
-            booked: false
-          },
-          "1400": {
-            time: '2:00pm',
-            booked: false
-          },
-          "1500": {
-            time: '3:00pm',
-            booked: false
-          }
-        }
-      
-}
+      }
+    }
+
     });    
 
   };
